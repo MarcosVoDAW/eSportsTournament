b31b1ec527baabf1d0256e90555dc3148856385d
Marcos Varela Orchillés <makarron453@gmail.com>
2025-02-25 12:36:10 +0100

    base

--- src/tournament/main/Main.java
+++ src/tournament/main/Main.java
@@ -1,5 +1,7 @@
+package tournament.main;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Probando23!");
+        System.out.println("Probando!");
     }
 }
